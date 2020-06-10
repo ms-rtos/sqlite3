cbbb4df4196e3acb1a7fd99da85662f4bb1867c3
jiaojinxing <jiaojinxing@acoinfo.com>
2020-06-10 11:04:22 +0800

    first commit

---
+++ README.md
@@ -0,0 +1,10 @@
+
+## sqlite3
+
+SQLite is a C-language library that implements a small, fast, self-contained, high-reliability, full-featured, SQL database engine. SQLite is the most used database engine in the world. SQLite is built into all mobile phones and most computers and comes bundled inside countless other applications that people use every day.
+
+License: Public Domain
+
+Download: `https://sqlite.org/download.html`
+
+Version: sqlite-amalgamation-3300100.zip 3.30.1
---
+++ src/ms_sqlite_porting.c
@@ -0,0 +1,28 @@
+/*
+ * Copyright (c) 2019 MS-RTOS Team.
+ * All rights reserved.
+ *
+ * Detailed license information can be found in the LICENSE file.
+ *
+ * File: ms_sqlite_porting.c SQLite porting.
+ *
+ * Author: Jiao.jinxing <jiaojixing@acoinfo.com>
+ *
+ */
+
+#include <ms_rtos.h>
+
+int chmod(const char *__path, mode_t __mode)
+{
+    return 0;
+}
+
+int fchmod(int fd, mode_t __mode)
+{
+    return 0;
+}
+
+int utimes(const char *__path, const struct timeval *__tvp)
+{
+    return 0;
+}
